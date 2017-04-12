0c5a396f514fee40473dcb369ffe1410844e1f8c
chienwei <chienwei0526@sina.com>
2017-04-12 19:21:18 +0800

    attack method & win/lose condition

--- app.js
+++ app.js
@@ -7,9 +7,40 @@ new Vue({
     },
     methods:{
         startGAME: function(){
-            this.gameisRunning = true
-            this.playerHealth = 100
-            this.monsterHealth = 100
-        }
+            this.gameisRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function(){
+            var max=10;
+            var min=3;
+            var damage= Math.max(Math.floor(Math.random()* max)+ 1 ,min);
+            this.monsterHealth -= damage;
+            
+            if(this.monsterHealth <= 0){
+                alert('YOU WON');
+                this.gameisRunning = false;
+                return;
+            } 
+            max = 12;
+            min = 5;
+            damage= Math.max(Math.floor(Math.random()* max)+ 1 ,min);
+            this.playerHealth -= damage;
+            
+            if(this.playerHealth <= 0){
+                alert('YOU LOSE');
+                this.gameisRunning = false; 
+            }
+            
+        },
+        specialAttack: function(){
+            
+        },
+        heal: function(){
+            
+        },
+        giveUp: function(){
+            
+        }, 
     }
 });
